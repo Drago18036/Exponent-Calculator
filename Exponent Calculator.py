4dad45a22ab0042a0e705d7b93dd6a43d16bf365
Drago18036 <104979222+Drago18036@users.noreply.github.com>
2022-05-05 21:47:17 +1200

    Add files via upload

---
+++ Exponent Calculator.py
@@ -0,0 +1,15 @@
+ValueError = True
+while ValueError:
+    try:
+        x = input("Base ")
+        x = int(x)
+        y = input("Exponent ")
+        y = int(y)
+        z = pow(x, y)
+        print(z)
+        ValueError = False
+    except:
+        print("Please input a number")
+
+        except:
+               ans == num 
